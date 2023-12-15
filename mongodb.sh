a8a2dba929b98bccd0bfe204210b90fd35386c66
Mohammed Asik <k.mohammed-asik@capgemini.com>
2023-12-15 18:41:01 +0530

    installing Mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -19,14 +19,14 @@ VALIDATE(){
 
     if [ $1 -ne 0 ]
     then
-        echo -e " $2..$R Failed "
+        echo -e " $2..$R Failed $N"
         exit 1  
     else 
-        echo -e " $2..$G success "
+        echo -e " $2..$G success $N"
     fi
 }
 
-yum list installed mongo-org 
+yum list installed mongodb-org-4.2.25-1.el8.x86_64 &>> $LOGFILE
     if [ $? -ne 0 ]
     then
 
